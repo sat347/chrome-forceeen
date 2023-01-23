0938dbe49f7859b13ebc97686efcb590bd3ea3db
sat347 <sat@fallmoon.jp>
2023-01-23 16:42:28 +0900

    Fixed language change not working

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 let choose_lang = document.getElementById('lang');
-let change_lang = document.getElementById('change');
 chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, (tabs) => {
     function changeLang() {
+        next_url = base_url = tabs[0].url.replace(/&hl=[a-zA-Z-]{2,5}/g, "");
         next_lang = document.getElementById('lang').value;
-        if (typeof cur_lang !== 'undefined' && cur_lang.length > 0 || next_lang.length == 0) {
-            chrome.tabs.update(tabs[0].id, { url: tabs[0].url.replace("&hl=" + cur_lang, "") })
-        } else if (typeof next_lang !== 'undefined') {
-            chrome.tabs.update(tabs[0].id, { url: tabs[0].url + '&hl=' + next_lang })
+        if (typeof next_lang !== 'undefined' && next_lang.length > 0) {
+            next_url += '&hl=' + next_lang;
         }
+        chrome.tabs.update(tabs[0].id, { url: next_url })
     }
     choose_lang.addEventListener('change', changeLang, false);
-    change_lang.addEventListener('click', changeLang, false);
 
     query = tabs[0].url.split("?")[1].split("&");
-    cur_lang = "";
     query.forEach(function (q) {
         if (q.split("=")[0] == "hl") {
             cur_lang = q.split("=")[1];
         }
     });
-    if (cur_lang != "") {
-        $("select.id").value = cur_lang;
+    if (typeof cur_lang !== 'undefined') {
+        document.getElementById("lang").querySelector("option[value='+cur_lang+']").setAttribute("selected", "selected");
     }
 })
